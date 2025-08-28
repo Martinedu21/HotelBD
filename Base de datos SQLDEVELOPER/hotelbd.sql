29b168b8083a498c19f00b7ea60e1d8206ed7f92
unknown <Cetecom@SJAPCA03LC0327.duoc.acad>
2025-08-28 18:36:07 -0400

    avanc e

---
+++ Base de datos SQLDEVELOPER/hotelbd.sql
@@ -0,0 +1,98 @@
+-- ==============================
+-- CREACIÓN DE TABLAS - GESTIÓN HOTEL
+-- ==============================
+
+-- TABLA HOTEL
+CREATE TABLE HOTEL (
+    id_hotel       NUMBER PRIMARY KEY,
+    nombre         VARCHAR2(100) NOT NULL,
+    direccion      VARCHAR2(200),
+    telefono       VARCHAR2(20),
+    email          VARCHAR2(100)
+);
+
+-- TABLA SERVICIO
+CREATE TABLE SERVICIO (
+    id_servicio     NUMBER PRIMARY KEY,
+    nombre_servicio VARCHAR2(50) NOT NULL,
+    descripcion     VARCHAR2(100),
+    precio_servicio NUMBER(10),
+    HOTEL_id_hotel  NUMBER,
+    CONSTRAINT SERVICIO_HOTEL_FK FOREIGN KEY (HOTEL_id_hotel) REFERENCES HOTEL(id_hotel)
+);
+
+-- TABLA HABITACION
+CREATE TABLE HABITACION (
+    id_habitacion   NUMBER PRIMARY KEY,
+    numero          VARCHAR2(10),
+    tipo            VARCHAR2(10),
+    precio_por_noche NUMBER,
+    estado          CHAR(1),
+    HOTEL_id_hotel  NUMBER,
+    CONSTRAINT HABITACION_HOTEL_FK FOREIGN KEY (HOTEL_id_hotel) REFERENCES HOTEL(id_hotel)
+);
+
+-- TABLA EMPLEADO
+CREATE TABLE EMPLEADO (
+    id_empleado     NUMBER(10) PRIMARY KEY,
+    nombre          VARCHAR2(30) NOT NULL,
+    apellido        VARCHAR2(30) NOT NULL,
+    cargo           VARCHAR2(30),
+    telefono        NUMBER,
+    email           VARCHAR2(100),
+    HOTEL_id_hotel  NUMBER,
+    CONSTRAINT EMPLEADO_HOTEL_FK FOREIGN KEY (HOTEL_id_hotel) REFERENCES HOTEL(id_hotel)
+);
+
+-- TABLA CLIENTE
+CREATE TABLE CLIENTE (
+    id_cliente      NUMBER PRIMARY KEY,
+    nombre          VARCHAR2(20) NOT NULL,
+    apellido        VARCHAR2(40) NOT NULL,
+    run             VARCHAR2(13) UNIQUE,
+    telefono        VARCHAR2(20),
+    email           VARCHAR2(100),
+    direccion       VARCHAR2(100)
+);
+
+-- TABLA RESERVA
+CREATE TABLE RESERVA (
+    id_reserva          NUMBER PRIMARY KEY,
+    fecha_inicio        DATE NOT NULL,
+    fecha_fin           DATE NOT NULL,
+    estado              CHAR(1),
+    CLIENTE_id_cliente  NUMBER,
+    HABITACION_id_habitacion NUMBER,
+    CONSUMO_SERVICIO_id_consumo NUMBER,
+    EMPLEADO_id_empleado NUMBER(10),
+    CONSTRAINT RESERVA_CLIENTE_FK FOREIGN KEY (CLIENTE_id_cliente) REFERENCES CLIENTE(id_cliente),
+    CONSTRAINT RESERVA_HABITACION_FK FOREIGN KEY (HABITACION_id_habitacion) REFERENCES HABITACION(id_habitacion),
+    CONSTRAINT RESERVA_EMPLEADO_FK FOREIGN KEY (EMPLEADO_id_empleado) REFERENCES EMPLEADO(id_empleado)
+);
+
+-- TABLA CONSUMO_SERVICIO
+CREATE TABLE CONSUMO_SERVICIO (
+    id_consumo          NUMBER PRIMARY KEY,
+    cantidad            NUMBER,
+    total               NUMBER,
+    RESERVA_id_reserva  NUMBER,
+    SERVICIO_id_servicio NUMBER,
+    CONSTRAINT CONSUMO_SERVICIO_RESERVA_FK FOREIGN KEY (RESERVA_id_reserva) REFERENCES RESERVA(id_reserva),
+    CONSTRAINT CONSUMO_SERVICIO_SERVICIO_FK FOREIGN KEY (SERVICIO_id_servicio) REFERENCES SERVICIO(id_servicio)
+);
+
+ALTER TABLE RESERVA
+ADD CONSTRAINT RESERVA_CONSUMO_SERVICIO_FK FOREIGN KEY (CONSUMO_SERVICIO_id_consumo)
+REFERENCES CONSUMO_SERVICIO(id_consumo);
+
+-- TABLA PAGO
+CREATE TABLE PAGO (
+    id_pago             NUMBER PRIMARY KEY,
+    RESERVA_id_reserva  NUMBER NOT NULL,
+    monto               NUMBER(10,2) NOT NULL,
+    fecha_de_pago       DATE NOT NULL,
+    metodo_pago         VARCHAR2(50),
+    estado              CHAR(1),
+    CONSTRAINT PAGO_RESERVA_FK FOREIGN KEY (RESERVA_id_reserva) REFERENCES RESERVA(id_reserva)
+);
+
